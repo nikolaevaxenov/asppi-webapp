907a735cdd4147884a5112518a220c096054428b
Ivan Nikolaev-Axenov <nikolaevaxenov@gmail.com>
2023-12-21 00:57:42 +0300

    Small improvements

--- asppi-backend/main.py
+++ asppi-backend/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import rc, cl, da1, wt
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(rc.router)
 app.include_router(cl.router)
